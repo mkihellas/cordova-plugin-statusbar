a99af033905738480a3a312093fe5a8c6826222e
mkihellas <35369394+mkihellas@users.noreply.github.com>
2018-10-17 17:05:48 +0300

    Update CDVStatusBar.m

--- src/ios/CDVStatusBar.m
+++ src/ios/CDVStatusBar.m
@@ -467,9 +467,9 @@ static const void *kStatusBarStyle = &kStatusBarStyle;
         }
     }
  
-    if ([self isIphoneX]) {
-        frame.size.height += self.webView.safeAreaInsets.bottom; 
-    }
+//     if ([self isIphoneX]) {
+//         frame.size.height += self.webView.safeAreaInsets.bottom; 
+//     }
  
     frame.size.height -= frame.origin.y;
     self.webView.frame = frame;
